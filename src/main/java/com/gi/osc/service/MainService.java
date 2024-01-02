ea61b09c2305c1d10f20e3de7cae23a4491620be
dlekgks <132041692+dlekgks@users.noreply.github.com>
2024-01-02 18:00:35 +0900

    상품 검색 기능까지 구현

--- src/main/java/com/gi/osc/service/MainService.java
+++ src/main/java/com/gi/osc/service/MainService.java
@@ -2,10 +2,12 @@ package com.gi.osc.service;
 
 import java.util.List;
 
+import com.gi.osc.bean.ProductDTO;
 import com.gi.osc.bean.StoreDTO;
 
 public interface MainService {
-	List<StoreDTO> getSearchList(StoreDTO dto);
+	public List<StoreDTO> getSearchList(StoreDTO dto);
+	public List<ProductDTO> searchProductsByName(ProductDTO dto);
 	
 
 }
--- src/main/java/com/gi/osc/service/MainServiceImpl.java
+++ src/main/java/com/gi/osc/service/MainServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.gi.osc.bean.ProductDTO;
 import com.gi.osc.bean.StoreDTO;
 import com.gi.osc.repository.MainMapper;
 
@@ -19,6 +20,12 @@ public class MainServiceImpl implements MainService {
 		// TODO Auto-generated method stub
 	
 		return mapper.searchList(dto);
+	}
+
+	@Override
+	public List<ProductDTO> searchProductsByName(ProductDTO dto) {
+		// TODO Auto-generated method stub
+		return mapper.searchProductsByName(dto);
 	}
 
 	
