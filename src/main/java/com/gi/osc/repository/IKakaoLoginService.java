21ccd4677a879574b04e9fa4b2b62067cbf8a553
hyejiiiii <148436328+hyejiiiii@users.noreply.github.com>
2024-01-10 10:08:17 +0900

    .

--- src/main/java/com/gi/osc/repository/IKakaoLoginService.java
+++ src/main/java/com/gi/osc/repository/IKakaoLoginService.java
@@ -7,4 +7,4 @@ public interface IKakaoLoginService {
 	String getAccessToken(String authorize_code) throws Throwable;
 
 	public HashMap<String, Object> getUserInfo(String access_Token) throws Throwable;
-}
+} 
